a89cb8a2d0bfd8c043bcc6b1768f5d285e791528
Grupo de Investigación OPALO <grupo.opalo2016@gmail.com>
2019-09-10 10:48:58 -0500

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,21 +1,22 @@
-#'Ejercicio b·sico de an·lisis de regresiÛn simple. Tomado de Montgomery y modificado por https://github.com/eitt
+#'Ejercicio b√°sico de an√°lisis de regresi√≥n simple. Tomado de Montgomery y modificado por https://github.com/eitt
 #'La Resistencia al desprendimiEnto depende de la Longitud del alambre, la Altura de la matriz, la Altura del poste y la Altura del amarre 
 #' Y ~ X1+X2+X3+X4 [Resist_ despren	~	Long_alambre+Alt_matriz+Alt_ poste+Alt_amarre]
-#Carga de librerÌas
+#Carga de librer√≠as
 library(lattice)
 library(ggplot2)
 library(graphics)
 library(corrplot)
+
 #'#Carga del archivo
-DATA <- read_delim("C:/Users/LEONARDO/Google Drive/MAESTRIA/SEGUNDO SEMESTRE/MACHINE LEARNING/MONTGOMERY.csv", 
+DATA <- read_delim("MONTGOMERY.csv", 
                    ";", escape_double = FALSE, trim_ws = TRUE)
 #'Resumen descriptivo de las variables
 summary(DATA)
-#'VisualizaciÛn de los datos
+#'Visualizaci√≥n de los datos
 View(DATA)
-#'Gr·fica de sacatter de las combinaciones de variables predictoras
+#'Gr√°fica de sacatter de las combinaciones de variables predictoras
 splom(DATA[2:5])
-#'Gr·fica de correlaciÛn
+#'Gr√°fica de correlaci√≥n
 M<-cor(DATA[2:5])
 corrplot(M, type="lower")
 Y=DATA[,1]
@@ -24,7 +25,7 @@ X2=DATA[,3]
 X3=DATA[,4]
 X4=DATA[,5]
 
-#'Se realiza un an·lisis de regresiÛn simple y m˙ltiple para las posibles combinaciones de Xi con la variable Y
+#'Se realiza un an√°lisis de regresi√≥n simple y m√∫ltiple para las posibles combinaciones de Xi con la variable Y
 fitA <-lm(Y ~ X1,data = DATA)
 summary(fitA)
 fitB <-lm(Y ~ X2,data = DATA)
